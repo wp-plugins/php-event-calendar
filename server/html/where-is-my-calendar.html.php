f8c475e8e74329f6003fd751b734b83f626bef15
chenster <chenster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 04:06:34 +0000

    1.4.2 lite release tag
    git-svn-id: https://plugins.svn.wordpress.org/php-event-calendar/trunk@1019987 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ server/html/where-is-my-calendar.html.php
@@ -0,0 +1,11 @@
+<h2>Don't see your calendar on your Web page?</h2>
+
+<p>
+Please make sure to include the following shortcode on your page.<br /><br />
+<code>
+[php_event_calendar]
+</code>
+</p>
+
+<br />
+<a href="https://wordpress.org/plugins/php-event-calendar/faq/" target="_new">Check out the complete FAQ.</a>
